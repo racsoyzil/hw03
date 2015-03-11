5eeceb336dc08a60c8ea8bb9909800b7d92c8320
racsoyzil <osckarok@yahoo.com>
2015-03-10 22:49:42 -0600

    homework 03
    declaramos tres tipos de datos y hacemos conversiones de estos a
    diferentes tipos de datos, e igulamos los valores

--- hw_01_id_14.cpp
+++ hw_01_id_14.cpp
@@ -1,19 +1,23 @@
-// Author:
-// Date:
-// Version:
-
-/*
-Homework DD
-this assignment  ...
-*/
-
-#include <iostream>
-
-int main ()
-{
-  // TODO your code here!!!!
-
-  std::cout<<"Hello world!!" <<std::endl;
-
-  return 0;
+//Autor:Oscar GArcia Avila
+//Date: 2/22/2015
+//Version:01
+//Homework  03, about casting between diferent data types
+//en esta tarea se declararan tres tipos diferentes de variables y se calculara el promedio 
+#include <iostream>//biblioteca
+int main(){
+	float c=5.4;//declaramos una variable de tipo decimal
+	double x=10.53;//declaramos variable de tipo entero
+    int b=x;//declaramos variable de tipo entero y lo igualamos al valor de x
+    std::cout<<"b esta declarado como b=x solo que x es de tipo double y b entero"<<std::endl;
+    std::cout<<"valor c:  "<< c<<std::endl;
+	std::cout<<"valor b:  "<< b<<std::endl;//se imprime el valor de cada dato al inicio
+	std::cout<<"valor x:  "<< x<<std::endl;
+	std::cout<<"si igualamos el valor de x=c nos da como resultado"<<std::endl;
+    x=c;//igualamos x a c
+    std::cout<<"valor x: "<< x<<std::endl;//se imprime el nuevo valor de x
+   	std::cout<<"si igualamos el valor de b=c nos da como resultado"<<std::endl;
+    b=c;
+    std::cout<<"valor b: "<< b<<std::endl;//se imprime el nuevo valor de b
+    system("PAUSE");//pausa en el sistema
+    return 0;//esto regresa un valor de cero
 }
